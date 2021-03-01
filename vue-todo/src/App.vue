4aaa01661f03f0705d374e6e8b59db5df5c96754
jooyoung <cwd3469@naver.com>
2021-03-01 18:09:48 +0900

    first commit

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Todoheader></Todoheader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <Todofooter></Todofooter>
+    
+  </div>
+</template>
+
+<script>
+import Todoheader from './components/header.vue'
+import TodoInput from './components/Todoinput.vue'
+import TodoList from './components/Todolist.vue'
+import Todofooter from './components/footer.vue'
+/*es6 문법 */
+export default {
+  components:{
+    'Todoheader':Todoheader,
+    'TodoInput':TodoInput,
+    'TodoList':TodoList,
+    'Todofooter':Todofooter,
+  }
+}
+</script>
+
+<style>
+
+</style>
