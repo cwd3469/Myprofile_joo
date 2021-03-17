770c93a8e51c4b68e6d745bef1949077ece9dfdd
dogmaster <cwd3469@naver.com>
2021-03-17 13:06:35 +0900

    TodoInput 컴포넌트 코드 정리 및 UI 스타일링
    1:33

--- vue-todo/src/components/Todoinput.vue
+++ vue-todo/src/components/Todoinput.vue
@@ -1,12 +1,40 @@
 <template>
   <div>
-      input
+      <input type="text" v-model="newTodoItem">
+      <button v-on:click="addTodo">add</button>
   </div>
+<!--인풋 박스을 만들어서 특정 택스트 값을 입력을 하면 그 택스트 값을 로컬스트로지에 저장을 합니다.-->
+<!--인풋 박스에 입력된 값들이 스크립트딴에서 인식을 할려면 v-model이라는 디렉티브를 사용해야한다. v-model의 역할은 입력된값은 동적으로 vue 인스턴트 안에 맵핑하는 역할-->
+<!--v-on:click의 역할 클릭했을때 동작하는 매소드를 입력하는 곳-->
 </template>
 
 <script>
 export default {
+  data: function(){
+    return{
+      newTodoItem:""
+    }
+  },
+  methods:{
+    addTodo: function(){
+     /*data 안에 있는 newTodoItem을 접근한다. 보통 this는 자바스크립트 스코프를 지칭하는것이다.
+     하지만 여기에서의 this는 data와 methods가 들어간 이 인스턴스를 말하는것이고 접근할수 있다.*/       
+    //  console.log(this.newTodoItem);
+     /*console.log의 저장하는 로직 */
+    
+     localStorage.setItem(this.newTodoItem,this.newTodoItem)
+     /*로컬스토리지에 저장하는 로직 
+     localStorage.setItem('key','value')키와 벨류로 setItem를 호출 할수 있습니다.*/
+    this.newTodoItem = '';
+    /* 함수입력 값를 초기화 하는 로직 newTodoItem를 비워 두면 클릭시 newTodoItem을 수행하고 있던 곳이 비워진다.*/
+    this.clearInput()
+    },
+    clearInput: function(){
+      this.newTodoItem = '';
+    }
 
+
+  }
 }
 </script>
 
--- vue-todo/src/components/header.vue
+++ vue-todo/src/components/header.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
-      header
-  </div>
+<header>
+  <h1>TODO it!</h1>
+</header>
 </template>
 
 <script>
@@ -10,6 +10,24 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+body{
+  text-align: center;
+  background-color: #f6f6f6;
+}
+h1{
+  color: #2f3b52;
+  font-weight: 900;
+  margin: 2.5rem 0 1.5rem;
+}
+input{
+  border-style: groove;
+  width: 200px;
+}
+button{
+  border-style: groove;
+}
+.shadow{
+  box-shadow: 5px 10px 10px rgba(0,0,0,0.03);
+}
 </style>
