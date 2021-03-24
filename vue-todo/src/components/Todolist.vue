27916aa396cc1279a3420f6940ebf2b587a75a67
Moung_master <cwd3469>
2021-03-25 07:53:20 +0900

    Todo footer 프로젝트 기본 구현 끝

--- vue-todo/src/components/Todolist.vue
+++ vue-todo/src/components/Todolist.vue
@@ -1,9 +1,15 @@
 <template>
   <div>
       <ul>
-        <li class="shadow" v-for="todoItem in todoItems" v-bind:key="todoItem">
-          {{todoItem}}
-          <span class="removeBtn " v-on:click="removeTodo">
+        <li class="shadow" v-for="(todoItem, index) in todoItems" v-bind:key="todoItem.item">
+          <!--todoitems 안에서 각각의 리스트를 todoItem 왕 index로 접근할 수 있다. 그리고 각각의 값을 매서드에 넘길수 있다.-->
+          <i class="fas fa-check checkBtn" v-on:click="toggleComplete(todoItem, index)" v-bind:class="{checkBtnCompleted:todoItem.completed}"></i>
+
+          <span v-bind:class="{textCompleted: todoItem.completed}">
+              {{ todoItem.item }}
+          </span>
+          <span class="removeBtn " v-on:click="removeTodo(todoItem, index)">
+           
             <i class="fas fa-trash-alt"></i>
           </span>
         </li>
@@ -26,8 +32,8 @@ export default {
         /*같은 인스턴스 안에 있는 todoItems에  push 담는다. */
         if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
         //localStorage.key(i)에 loglevel:webpack-dev-server를 제외한 나머지 값
-          this.todoItems.push(localStorage.key(i))
-        // console.log( localStorage.key(i));
+          this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+          //getItem로 localStorage.key를 뺀다면 벨류가 떨어져 나간다. input에서 저장될때 json.string으로 보냈기 때문에 순수하게 들어오는 값도 string이기 때문에 parse(풀어서) obj으로 바꾸어준다.
         }
 
       }
@@ -35,9 +41,16 @@ export default {
     }
   },
   methods:{
-    removeTodo:function(){
-      
-    }
+    removeTodo:function(todoItem, index){
+      localStorage.removeItem(todoItem)
+      this.todoItems.splice(index,1)
+    },
+    toggleComplete: function(todoItem, index){
+      todoItem.completed = !todoItem.completed;
+      //로컬 스토리지의 데이터를 갱신한다.
+      localStorage.removeItem(todoItem, index)
+      localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+    },
   },
 }
 </script>
